8f29fbbe6b4b580381b91594179ff1bc60619384
Angel Hurtado <contact@angelxehg.com>
2021-03-01 10:18:43 -0600

    Configurar nombre del sitio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Angel Hurtado`,
+    description: `Mi sitio web y blog personal`,
+    author: `@angelxehg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
