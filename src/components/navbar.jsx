7af8499b31543915bdb11e482e2cc9fbd774b0a5
Angel Hurtado <contact@angelxehg.com>
2021-04-10 19:11:18 -0500

    Usar React Bootstrap para Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,73 +1,32 @@
 import React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
-
-export const Navbar = props => (
-  <nav className="navbar navbar-expand-sm navbar-dark sticky-top pg-bg divr">
-    <div className="container-fluid">{props.children}</div>
-  </nav>
-)
-
-Navbar.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-const NavbarBrand = props => (
-  <Link className="navbar-brand" to={props.to} style={{ color: "white" }}>
-    {props.title}
-  </Link>
-)
-
-NavbarBrand.propTypes = {
-  title: PropTypes.string.isRequired,
-  to: PropTypes.string.isRequired,
-}
-
-const NavbarToggler = () => (
-  <button
-    className="navbar-toggler"
-    type="button"
-    data-bs-toggle="collapse"
-    data-bs-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
-    <span className="navbar-toggler-icon"></span>
-  </button>
-)
-
-const NavbarCollapse = props => (
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav">{props.children}</ul>
-  </div>
-)
-
-NavbarCollapse.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-const NavbarItem = props => (
-  <li className="nav-item">
-    <Link className="nav-link" to={props.to}>
-      {props.title}
-    </Link>
-  </li>
-)
-
-NavbarItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  to: PropTypes.string.isRequired,
-}
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from "react-bootstrap/Container";
 
 const DefaultNavbar = () => (
-  <Navbar>
-    <NavbarBrand title="Portafolio de Angel" to="/projects" />
-    <NavbarCollapse>
-      {/* <NavbarItem title="Blog" to="https://blog.angelxehg.com/" /> */}
-      <NavbarItem title="Resumen" to="/" />
-      <NavbarItem title="Acerca de" to="/about" />
-    </NavbarCollapse>
+  <Navbar bg="dark" variant="dark" expand="sm" sticky="top" className="pg-bg divr">
+    <Container fluid="sm">
+      <Link className="navbar-brand" to="/projects">
+        Proyectos de Angel
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Item>
+            <Link className="nav-link" to="/">
+              Resumen
+            </Link>
+          </Nav.Item>
+          {/* <Nav.Link href="https://blog.angelxehg.com/">Blog</Nav.Link> */}
+          <Nav.Item>
+            <Link className="nav-link" to="/about">
+              Acerca de
+            </Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 )
 
