9ab4fa26c3f14c4816f277fea2a51e208366edb0
Angel Hurtado <sudo@angelxehg.com>
2021-05-07 12:40:33 -0500

    Mover lista de proyectos y stacks a /meta

---
+++ src/meta/stacks.js
@@ -0,0 +1,20 @@
+const stacks = [
+  {
+    title: "Lenguajes",
+    icons: ["HTML5", "CSS3", "Javascript", "Typescript", "Python", "Markdown"],
+  },
+  {
+    title: "Frameworks y librerias",
+    icons: ["React", "Ionic", "Gatsby", "Django", "Bootstrap", "Angular"],
+  },
+  {
+    title: "Herramientas",
+    icons: ["Ubuntu", "VSCode", "NodeJS", "NPM", "Git"],
+  },
+  {
+    title: "Plataformas",
+    icons: ["Netlify", "GitHub", "GitLab", "Firebase", "DigitalOcean"],
+  },
+]
+
+export default stacks;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,8 @@ import SEO from "@components/seo"
 import { LinkExternal, ProfileImage, socialLinks } from "@components/social"
 import { availableIcons, Icon, LinkIcon } from "@components/icons"
 import { useSiteMetadata } from "@hooks/use-site-metadata"
+import projects from "../meta/projects"
+import stacks from "../meta/stacks"
 
 const PageHeader = () => {
   const site = useSiteMetadata()
@@ -32,39 +34,6 @@ const PageHeader = () => {
     </div>
   )
 }
-
-const projects = [
-  {
-    slug: "portafolio",
-    title: "Portafolio profesional",
-    extract: "Mi sitio web y portafolio profesional",
-    stack: "Gatsby,React,Javascript,GitHub",
-    github: "https://github.com/angelxehg/angelxehg.github.io",
-    demo: "https://angelxehg.github.io/",
-  },
-  {
-    slug: "react-capacitor",
-    title: "React + Capacitor",
-    extract: "Ejemplo de aplicación hibrida sin Ionic",
-    stack: "React,Android,GitHub",
-    github: "https://github.com/angelxehg/react-examples",
-    demo: "https://react-capacitor-examples.netlify.app/",
-  },
-  {
-    slug: "tomatoe-chat",
-    title: "Tomatoe Chat",
-    extract: "Aplicación móvil de chat",
-    stack: "Ionic,Android,Angular,Javascript,Typescript,Firebase",
-    github: "https://github.com/angelxehg/tomatoe-chat",
-  },
-  {
-    slug: "xpress-library",
-    title: "Xpress Library",
-    extract: "Administración de una biblioteca",
-    stack: "Angular,Bootstrap,Javascript,Typescript,NodeJS",
-    github: "https://github.com/angelxehg/xpress-library",
-  },
-]
 
 const ProjectsSection = () => (
   <section id="projects">
@@ -128,25 +97,6 @@ const EducationSection = () => (
     </div>
   </section>
 )
-
-const stacks = [
-  {
-    title: "Lenguajes",
-    icons: ["HTML5", "CSS3", "Javascript", "Typescript", "Python", "Markdown"],
-  },
-  {
-    title: "Frameworks y librerias",
-    icons: ["React", "Ionic", "Gatsby", "Django", "Bootstrap", "Angular"],
-  },
-  {
-    title: "Herramientas",
-    icons: ["Ubuntu", "VSCode", "NodeJS", "NPM", "Git"],
-  },
-  {
-    title: "Plataformas",
-    icons: ["Netlify", "GitHub", "GitLab", "Firebase", "DigitalOcean"],
-  },
-]
 
 const SkillsSection = () => (
   <section id="skills">
