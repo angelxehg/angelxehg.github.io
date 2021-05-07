7e58471050611cb9859d751f12c21abb3cae3ab2
Angel Hurtado <sudo@angelxehg.com>
2021-05-07 11:31:12 -0500

    Usar nuevo page header en index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,52 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import CenterLayout from "@layouts/center"
+import DefaultFooter from "@components/footer"
+import DefaultNavbar from "@components/navbar"
 import SEO from "@components/seo"
 import { LinkExternal, ProfileImage, socialLinks } from "@components/social"
 import { Icon } from "@components/icons"
 import { useSiteMetadata } from "@hooks/use-site-metadata"
-import { Card, Col, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 
-const IndexPage = () => {
+const PageHeader = () => {
   const site = useSiteMetadata()
   return (
-    <CenterLayout>
-      <SEO title="Inicio" lang="es" />
-      <Card className="bg-dark pt-3">
+    <Row className="text-center text-sm-start">
+      <Col sm="auto">
         <ProfileImage />
-        <Card.Body>
-          <Link to="/">
-            <h1 className="card-title">{site.title}</h1>
-          </Link>
-          <Card.Text className="m-0">{site.description}</Card.Text>
-          <Row className="mt-2">
-            <Col>
-              <Link to="/projects" className="me-3">
-                Proyectos
-              </Link>
-              <Link to="/about">Acerca de</Link>
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col>
-              <LinkExternal
-                to="mailto:sudo@angelxehg.com"
-                title="Email (sudo@angelxehg.com)"
-              >
-                <Icon name="Email" size="1.5rem" />
-              </LinkExternal>
-              {socialLinks.map(({ title, to, icon }) => (
-                <LinkExternal key={to} to={to} title={title}>
-                  <Icon name={icon} size="1.5rem" />
-                </LinkExternal>
-              ))}
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    </CenterLayout>
+      </Col>
+      <Col>
+        <header className="m-sm-0 mt-2">
+          <h1>{site.title}</h1>
+          <p>{site.description}</p>
+        </header>
+        <LinkExternal to="mailto:sudo@angelxehg.com" title="Email">
+          <Icon name="Email" className="me-1" size="1.5rem" />
+          sudo@angelxehg.com
+        </LinkExternal>
+        <br className="d-block d-sm-none" />
+        {socialLinks.map(({ title, to, icon }) => (
+          <LinkExternal key={to} to={to} title={title}>
+            <Icon name={icon} size="1.5rem" />
+          </LinkExternal>
+        ))}
+      </Col>
+    </Row>
   )
 }
+
+const IndexPage = () => (
+  <div>
+    <SEO title="Portafolio" lang="es" />
+    <DefaultNavbar />
+    <Container fluid="md" className="pt-3 pb-3">
+      <PageHeader />
+    </Container>
+    <DefaultFooter />
+  </div>
+)
 
 export default IndexPage
