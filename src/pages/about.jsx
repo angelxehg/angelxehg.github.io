6568307c7019cdb0369279c82c430ac63f788d3b
Angel Hurtado <sudo@angelxehg.com>
2021-05-07 12:02:43 -0500

    Usar páginas about y projects como redirects

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,98 +1,24 @@
 import React from "react"
-import Container from "react-bootstrap/Container"
+import { navigate } from "gatsby"
 
+import CenterLayout from "../layouts/center"
 import SEO from "@components/seo"
-import { LinkExternal } from "@components/social"
-import { useSiteMetadata } from "@hooks/use-site-metadata"
-import { LinkIcon, Icon, availableIcons } from "@components/icons"
-import DefaultFooter from "@components/footer"
-import DefaultNavbar from "@components/navbar"
-import { Card, Col, Row } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 
-const stacks = [
-  {
-    title: "Lenguajes",
-    icons: ["HTML5", "CSS3", "Javascript", "Typescript", "Python", "Markdown"],
-  },
-  {
-    title: "Frameworks y librerias",
-    icons: ["React", "Ionic", "Gatsby", "Django", "Bootstrap", "Angular"],
-  },
-  {
-    title: "Herramientas",
-    icons: ["Ubuntu", "VSCode", "NodeJS", "NPM", "Git"],
-  },
-  {
-    title: "Plataformas",
-    icons: ["Netlify", "GitHub", "GitLab", "Firebase", "DigitalOcean"],
-  },
-]
+setTimeout(() => navigate('/'), 2000)
 
-const AboutPage = () => {
-  const site = useSiteMetadata()
-  return (
-    <div>
-      <SEO title="Acerca de mi" lang="es" />
-      <DefaultNavbar />
-      <Container fluid="md" className="pt-3 pb-3">
-        <header>
-          <h1>Acerca de mi</h1>
-          <p>{site.description}</p>
-        </header>
-        <section id="education">
-          <h2>Educación</h2>
-          <Row>
-            <Col xl="7" lg="8" className="p-md-1 pb-2">
-              <Card className="bg-dark">
-                <Card.Body>
-                  <Card.Title>
-                    Ingeniería en Tecnologías de la Información y Comunicación
-                  </Card.Title>
-                  <Card.Text className="m-0 mb-1">
-                    <LinkExternal to="http://www.utzac.edu.mx" title="UTZAC">
-                      <Icon name="Web" /> Universidad Tecnológica del Estado de
-                      Zacatecas
-                    </LinkExternal>
-                  </Card.Text>
-                  <Card.Text className="text-muted">
-                    Guadalupe, Zacatecas | 2017-2021
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </section>
-        <section id="skills" className="mt-3">
-          <h2>Habilidades</h2>
-          <Row>
-            {stacks.map(({ title, icons }, n) => (
-              <Col key={n} xl="4" md="6" className="p-md-1 pb-2">
-                <Card className="bg-dark">
-                  <Card.Body>
-                    <Card.Title>
-                      {title}
-                    </Card.Title>
-                    <Card.Text>
-                      {icons.map(icon => {
-                        if (availableIcons.find(i => i === icon)) {
-                          return (
-                            <LinkIcon key={icon} name={icon} size="1.5rem" />
-                          )
-                        } else {
-                          console.log(icon)
-                        }
-                      })}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </section>
-      </Container>
-      <DefaultFooter />
-    </div>
-  )
-}
+const AboutPage = () => (
+  <CenterLayout>
+    <SEO title="404: Not found" lang="es" />
+    <Card className="bg-dark">
+      <Card.Body>
+        <h1 className="card-title">301: Moved Permanently</h1>
+        <Card.Text className="m-0">
+          Redireccionando a página principal...
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </CenterLayout>
+)
 
 export default AboutPage
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,94 +1,25 @@
 import React from "react"
-import Container from "react-bootstrap/Container"
+import { navigate } from "gatsby"
 
-import DefaultFooter from "@components/footer"
+import CenterLayout from "../layouts/center"
 import SEO from "@components/seo"
-import { Icon, LinkIcon } from "@components/icons"
-import { LinkExternal } from "@components/social"
-import DefaultNavbar from "@components/navbar"
-import { Card, Col, Row } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 
-const projects = [
-  {
-    slug: "portafolio",
-    title: "Portafolio profesional",
-    extract: "Mi sitio web y portafolio profesional",
-    stack: "Gatsby,React,Javascript,GitHub",
-    github: "https://github.com/angelxehg/angelxehg.github.io",
-    demo: "https://angelxehg.github.io/",
-  },
-  {
-    slug: "react-capacitor",
-    title: "React + Capacitor",
-    extract: "Ejemplo de aplicación hibrida sin Ionic",
-    stack: "React,Android,GitHub",
-    github: "https://github.com/angelxehg/react-examples",
-    demo: "https://react-capacitor-examples.netlify.app/",
-  },
-  {
-    slug: "tomatoe-chat",
-    title: "Tomatoe Chat",
-    extract: "Aplicación móvil de chat",
-    stack: "Ionic,Android,Angular,Javascript,Typescript,Firebase",
-    github: "https://github.com/angelxehg/tomatoe-chat",
-  },
-  {
-    slug: "xpress-library",
-    title: "Xpress Library",
-    extract: "Administración de una biblioteca",
-    stack: "Angular,Bootstrap,Javascript,Typescript,NodeJS",
-    github: "https://github.com/angelxehg/xpress-library",
-  },
-]
+setTimeout(() => navigate('/#projects'), 2000)
 
 const ProjectsPage = () => {
   return (
-    <div>
-      <SEO title="Proyectos" lang="es" />
-      <DefaultNavbar />
-      <Container fluid="md" className="pt-3 pb-3">
-        <header>
-          <h1>Proyectos</h1>
-          <p>
-            Estos son mis proyectos que he realizado por mi cuenta, y como
-            proyectos escolares.
-          </p>
-        </header>
-        <Row>
-          {projects.map(item => {
-            const { slug, title, extract, stack, github, demo } = item
-            const stackIcons = stack.split(",")
-            return (
-              <Col key={slug} xl="4" md="6" className="p-1">
-                <Card className="bg-dark">
-                  <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text className="m-0">{extract}</Card.Text>
-                    <Card.Text className="text-muted">
-                      Hecho con:{" "}
-                      {stackIcons.map(icon => (
-                        <LinkIcon key={icon} name={icon} />
-                      ))}
-                    </Card.Text>
-                    {github && (
-                      <LinkExternal to={github} title="Repositorio">
-                        <Icon name="GitHub" /> Repositorio
-                      </LinkExternal>
-                    )}
-                    {demo && (
-                      <LinkExternal to={demo} title="Demo">
-                        <Icon name="Web" /> Demo
-                      </LinkExternal>
-                    )}
-                  </Card.Body>
-                </Card>
-              </Col>
-            )
-          })}
-        </Row>
-      </Container>
-      <DefaultFooter />
-    </div>
+    <CenterLayout>
+      <SEO title="404: Not found" lang="es" />
+      <Card className="bg-dark">
+        <Card.Body>
+          <h1 className="card-title">301: Moved Permanently</h1>
+          <Card.Text className="m-0">
+            Redireccionando a Proyectos...
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CenterLayout>
   )
 }
 
