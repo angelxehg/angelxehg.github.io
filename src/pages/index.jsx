14d43d7c2dfe92124513e630f3d52cbaef9406fd
Angel Hurtado <sudo@angelxehg.com>
2021-05-12 13:56:35 -0500

    Crear Card especifico para links de redes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import DefaultFooter from "../components/footer"
 import SEO from "../components/seo"
@@ -18,30 +17,46 @@ const PageHeader = () => {
       </div>
       <div className="col">
         <header className="m-sm-0 mt-2">
-          <Link to="/">
-            <h1 className="h3">{site.title}</h1>
-          </Link>
+          <h1 className="h3">{site.title}</h1>
           <p>{site.description}</p>
         </header>
-        <div className="row">
-          <div className="col-sm-auto">
-            <LinkExternal to="mailto:sudo@angelxehg.com" title="Email">
-              <Icon name="Email" className="me-1" size="1.5rem" />
-              sudo@angelxehg.com
-            </LinkExternal>
-          </div>
-          <div className="col-sm-auto mt-sm-0 mt-2">
-            {socialLinks.map(({ title, to, icon }) => (
-              <LinkExternal key={to} to={to} title={title}>
-                <Icon name={icon} size="1.5rem" />
-              </LinkExternal>
-            ))}
-          </div>
-        </div>
       </div>
     </div>
   )
 }
+
+const ContactSection = () => (
+  <section id="contact" className="mt-3">
+    <h2 className="h4">Acerca de</h2>
+    <div className="row">
+      <div className="col p-md-1 pb-2">
+        <div className="card bg-dark">
+          <div className="card-body">
+            <h3 className="h5 card-title">Contacto</h3>
+            <ul className="p-0" style={{ listStyleType: 'none' }}>
+              <li>
+                <LinkExternal to="mailto:sudo@angelxehg.com" title="Email">
+                  <Icon name="Email" className="me-2" size="1.5rem" />
+                  sudo@angelxehg.com
+                </LinkExternal>
+              </li>
+              {socialLinks.map(({ title, to, icon }) => {
+                return (
+                  <li key={to} className="mt-1" style={{ listStyle: 'none' }}>
+                    <LinkExternal to={to} title={title}>
+                      <Icon name={icon} className="me-2" size="1.5rem" />
+                      {title}
+                    </LinkExternal>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
 
 const ProjectsSection = () => (
   <section id="projects" className="mt-3">
@@ -144,7 +159,8 @@ const IndexPage = () => (
     <div className="container-md pt-4 pb-3">
       <PageHeader />
       <div className="row">
-        <div className="col-lg-4 order-lg-last">
+        <div className="col-lg-4">
+          <ContactSection />
           <EducationSection />
         </div>
         <div className="col-lg">
