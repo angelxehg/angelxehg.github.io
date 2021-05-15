fcff1c3d4e0a05d57c43214ac9d2cc905f590995
Angel Hurtado <sudo@angelxehg.com>
2021-05-14 20:48:50 -0500

    Añadir Avaaatar concerned a páginas 404 y redirecciones

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,10 +4,13 @@ import { Link } from "gatsby"
 import CenterLayout from "../layouts/center"
 import SEO from "../components/seo"
 
+const ConcernedSVG = require("../assets/concerned.svg")
+
 const NotFoundPage = () => (
   <CenterLayout>
     <SEO title="404: Not found" lang="es" />
     <div className="card bg-dark">
+      <ConcernedSVG style={{ width: '5rem', height: '5rem' }} className="align-self-center mt-2" />
       <div className="card-body">
         <h1 className="h3 card-title">404 Not Found</h1>
         <p className="card-text m-0">La página que buscas no existe</p>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,12 +4,15 @@ import { Link, navigate } from "gatsby"
 import CenterLayout from "../layouts/center"
 import SEO from "../components/seo"
 
+const ConcernedSVG = require("../assets/concerned.svg")
+
 setTimeout(() => navigate("/"), 2000)
 
 const AboutPage = () => (
   <CenterLayout>
     <SEO title="301: Moved Permanently" lang="es" />
     <div className="card bg-dark">
+      <ConcernedSVG style={{ width: '5rem', height: '5rem' }} className="align-self-center mt-2" />
       <div className="card-body">
         <h1 className="h3 card-title">301: Moved Permanently</h1>
         <p className="card-text m-0">
