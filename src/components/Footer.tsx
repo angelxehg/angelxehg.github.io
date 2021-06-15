23ba0a3a2d05b0e44a808af50a44d5fc95d5c203
Angel Hurtado <sudo@angelxehg.com>
2021-06-15 15:53:34 -0500

    Pass reset prop to links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,15 +15,15 @@ const DefaultFooter = (): JSX.Element => (
   <Navbar as="footer" variant="dark" bg="dark" className="mt-3">
     <Container style={{ display: "block" }}>
       <p className="navbar-text m-0">
-        © {new Date().getFullYear()}, Angel Hurtado. Portafolio profesional
-        creado con <IconLink meta={gatsbyLinkMeta} /> y{" "}
-        <IconLink meta={bootstrapLinkMeta} />. Reporta problemas o envia tus
-        sugerencias a la página <IconLink meta={gitHubIssues} />. Versión del
-        sitio web: <IconLink meta={gitHubRepoVer} />
+        © {new Date().getFullYear()}, Angel Hurtado @{" "}
+        <IconLink meta={gitHubRepoVer} reset />. Portafolio profesional creado
+        con <IconLink meta={gatsbyLinkMeta} reset /> y{" "}
+        <IconLink meta={bootstrapLinkMeta} reset />. Reporta problemas o envia
+        tus sugerencias a la página <IconLink meta={gitHubIssues} reset />.
       </p>
       <p className="navbar-text m-0">
         Las marcas y logotipos presentados en este sitio web pertenecen a sus
-        respectivos dueños. Iconos de <IconLink meta={fasLinkMeta} />
+        respectivos dueños. Iconos de <IconLink meta={fasLinkMeta} reset />
       </p>
     </Container>
   </Navbar>
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,10 +3,18 @@ import React from "react"
 import Icon from "./Icon"
 import { LinkMeta } from "../meta/links"
 
-const Link = (props: { meta: LinkMeta }) => {
+const Link = (props: {
+  meta: LinkMeta
+  reset?: boolean
+  className?: string
+}) => {
+  const classes = props.className ? props.className.split(" ") : []
+  if (props.reset) {
+    classes.push("text-reset")
+  }
   const { href, name, icon } = props.meta
   return (
-    <a href={href} title={name} rel="external">
+    <a href={href} title={name} className={classes.join(" ")} rel="external">
       {name} <Icon meta={icon} />
     </a>
   )
