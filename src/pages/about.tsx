35eb02482b14c4032698fd8a24851408558ef311
Angel Hurtado <sudo@angelxehg.com>
2021-06-16 12:42:59 -0500

    Mi ubicación

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,7 +17,7 @@ export const AboutSection = () => (
           <div className="card-body">
             <h3 className="card-title h5">Trayectoria</h3>
             <p className="card-text m-0">
-              Mi travesía en el mundo de la tecnología comienza en 2014, cuando comencé a crear aplicaciones para Windows Phone y Windows. Para lograr esto aprendí Visual Basic, y posteriormente C# y XAML.
+              Mi travesía en el mundo de la tecnología comienza en 2014, cuando comencé a crear aplicaciones para Windows Phone y Windows. Para lograr esto aprendí Visual Basic, y posteriormente C# y XAML. Realicé algunos proyectos que me ayudaron a entender muchas de las bases de la programación.
             </p>
             <p className="card-text m-0 mt-2">
               Fue hasta en la Universidad donde comencé a aprender y trabajar con tecnologías web. Me enseñaron HTML, PHP, Python, JavaScript, así como algunos frameworks como Laravel. Tambien durante este tiempo aprendí por mi cuenta Angular, React y Firebase, y descubrí que lo mio es el desarrollo front-end.
@@ -36,6 +36,21 @@ export const AboutSection = () => (
               <li>
                 Ingeniería en Tecnologías de la Información y Comunicación{" "}
                 <br /><UTZACLink /> 2017 - 2021
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 p-sm-1 pb-2">
+        <div className="card bg-dark text-light">
+          <div className="card-body">
+            <h3 className="card-title h5">Ubicación</h3>
+            <ul className="p-0 m-0" style={{ listStyleType: "none" }}>
+              <li>
+                <b>Origen</b>: Fresnillo Zacatecas, México
+              </li>
+              <li>
+                <b>Idiomas</b>: Español (nativo), Inglés (intermedio)
               </li>
             </ul>
           </div>
