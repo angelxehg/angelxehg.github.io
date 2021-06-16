77689311d82efd268edac2f48c4d8b118567b7cf
Angel Hurtado <sudo@angelxehg.com>
2021-06-16 12:38:31 -0500

    Descripción de trayectoria

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ import Redirect from "../components/Redirect"
 
 const UTZACLink = () => (
   <a href="http://www.utzac.edu.mx" rel="external" title="UTZAC">
-    UTZAC
+    Universidad Tecnológica del Estado de Zacatecas
   </a>
 )
 
@@ -15,11 +15,27 @@ export const AboutSection = () => (
       <div className="col-12 p-sm-1 pb-2">
         <div className="card bg-dark text-light">
           <div className="card-body">
+            <h3 className="card-title h5">Trayectoria</h3>
+            <p className="card-text m-0">
+              Mi travesía en el mundo de la tecnología comienza en 2014, cuando comencé a crear aplicaciones para Windows Phone y Windows. Para lograr esto aprendí Visual Basic, y posteriormente C# y XAML.
+            </p>
+            <p className="card-text m-0 mt-2">
+              Fue hasta en la Universidad donde comencé a aprender y trabajar con tecnologías web. Me enseñaron HTML, PHP, Python, JavaScript, así como algunos frameworks como Laravel. Tambien durante este tiempo aprendí por mi cuenta Angular, React y Firebase, y descubrí que lo mio es el desarrollo front-end.
+            </p>
+            <p className="card-text m-0 mt-2">
+              Ahora acabo de terminar mi carrera en la <UTZACLink />, pero continuo aprendiendo <a href="#skill">nuevas herramientas</a> y creando <a href="#projects">proyectos</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 p-sm-1 pb-2">
+        <div className="card bg-dark text-light">
+          <div className="card-body">
             <h3 className="card-title h5">Educación</h3>
             <ul className="p-0 m-0" style={{ listStyleType: "none" }}>
               <li>
                 Ingeniería en Tecnologías de la Información y Comunicación{" "}
-                <br />@ <UTZACLink /> 2017-2021
+                <br /><UTZACLink /> 2017 - 2021
               </li>
             </ul>
           </div>
