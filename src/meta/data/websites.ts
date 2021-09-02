712a53c1d603c18d4233f17b97e17c306946c5b3
Angel Hurtado <sudo@angelxehg.com>
2021-09-02 12:49:26 -0500

    Remove colors from links

--- src/meta/data/websites.ts
+++ src/meta/data/websites.ts
@@ -4,7 +4,6 @@ const websites: LinkMeta[] = [
   {
     name: "Instagram",
     icon: {
-      style: { color: "#F70038" },
       svgPath: "bootstrap-icons/instagram.svg",
     },
     href: "https://instagram.com/",
@@ -12,11 +11,6 @@ const websites: LinkMeta[] = [
   {
     name: "LinkedIn",
     icon: {
-      style: {
-        color: "#2563AC",
-        backgroundColor: "white",
-        borderRadius: "15%",
-      },
       svgPath: "bootstrap-icons/linkedin.svg",
     },
     href: "https://www.linkedin.com/",
@@ -24,7 +18,6 @@ const websites: LinkMeta[] = [
   {
     name: "PayPal",
     icon: {
-      style: { color: "#002E82" },
       svgPath: "seek-logo/paypal.svg",
     },
     href: "https://paypal.com/",
@@ -32,7 +25,6 @@ const websites: LinkMeta[] = [
   {
     name: "Twitter",
     icon: {
-      style: { color: "#1C9CEA" },
       svgPath: "bootstrap-icons/twitter.svg",
     },
     href: "https://www.twitter.com/",
