e3ec0b85bf1abd2fd500de635794276fe841dda6
Angel Hurtado <angelxehg@proton.me>
2023-11-25 17:17:00 -0600

    fix: Change updated at

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const DefaultFooter = (): JSX.Element => (
   <footer className="navbar bg-body-secondary">
     <div className="container-lg ps-sm-4 pe-sm-4" style={{ display: "block" }}>
       <p className="navbar-text m-0">
-        Angel Hurtado © {new Date().getFullYear()}. Actualizado el 21 noviembre
+        Angel Hurtado © {new Date().getFullYear()}. Actualizado el 25 noviembre
         de 2023. Reporta problemas o envía tus sugerencias en{" "}
         <a
           href="https://github.com/angelxehg/angelxehg.github.io/issues"
