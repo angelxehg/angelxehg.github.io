2c200de59c27e805b9c4214a6ca2cfc38de908d7
Angel Hurtado <angelxehg@proton.me>
2025-05-18 15:29:13 -0600

    build: Set previous robots config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-console.log("BUILD")
-console.log("BUILD NETLIFY", process.env.NETLIFY)
-const isNetlify = process.env.NETLIFY === 'true'
-console.log('isNetlify', isNetlify)
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   ssr: true,
@@ -13,5 +8,5 @@ export default defineNuxtConfig({
   },
   devtools: {enabled: true},
   modules: ['@nuxtjs/robots'],
-  site: {indexable: true} // Disable indexing on Netlify
+  site: {indexable: !process.env.NETLIFY} // Disable indexing on Netlify
 });
