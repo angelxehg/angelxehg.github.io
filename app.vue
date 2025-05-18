7cfdf86f7b1c6e2969d63f1f8774f3bada132561
Angel Hurtado <angelxehg@proton.me>
2025-05-18 13:14:41 -0600

    feat: Very basic links

--- app.vue
+++ app.vue
@@ -1,8 +1,38 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1>Angel Hurtado</h1>
+  <p>Site is under construction! check my links:</p>
+  <ul>
+    <li>
+      <a href="mailto:angelxehg@proton.me" rel="external" title="Email (angelxehg@proton.me)">
+        Email (angelxehg@proton.me)
+      </a>
+    </li>
+    <li>
+      <a href="https://www.linkedin.com/in/angelxehg" rel="external" title="LinkedIn (/in/angelxehg)">
+        LinkedIn (/in/angelxehg)
+      </a>
+    </li>
+    <li>
+      <a href="https://github.com/angelxehg" rel="external" title="GitHub (@angelxehg)">
+        GitHub (@angelxehg)
+      </a>
+    </li>
+    <li>
+      <a href="https://dev.to/angelxehg" rel="external" title="Dev.to (@angelxehg)">
+        Dev.to (@angelxehg)
+      </a>
+    </li>
+    <li>
+      <a href="https://threads.net/angelxehg" rel="external" title="Threads (@angelxehg)">
+        Threads (@angelxehg)
+      </a>
+    </li>
+    <li>
+      <a href="https://instagram.com/angelxehg" rel="external" title="Instagram (@angelxehg)">
+        Instagram (@angelxehg)
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script setup>
@@ -15,10 +45,20 @@ useHead({
     { name: 'og:description', content: "Angel Hurtado's portfolio description" },
     { name: 'og:type', content: 'website' },
     { name: 'og:image', content: 'images/OpenGraph.Opt.png' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { name: 'google-site-verification', content: '0hO20fg4BEvRMjuTmUHifKltClQKkc4KKlwGD9tg6' }
   ],
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-  ]
+  ],
+  htmlAttrs: {
+    lang: 'en',
+  }
 })
 </script>
+
+<style>
+:root {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
