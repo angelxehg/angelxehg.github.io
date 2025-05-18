eaf5f2444cef1fb6129aa277704c031009a919a8
Angel Hurtado <angelxehg@proton.me>
2025-05-18 13:01:22 -0600

    feat: Add header tags

--- app.vue
+++ app.vue
@@ -4,3 +4,21 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: "Angel Hurtado's portfolio",
+  meta: [
+    { name: 'description', content: "Angel Hurtado's portfolio description" },
+    { name: 'og:title', content: "Angel Hurtado's portfolio" },
+    { name: 'og:site_name', content: "Angel Hurtado's portfolio" },
+    { name: 'og:description', content: "Angel Hurtado's portfolio description" },
+    { name: 'og:type', content: 'website' },
+    { name: 'og:image', content: 'images/OpenGraph.Opt.png' },
+    { name: 'google-site-verification', content: '0hO20fg4BEvRMjuTmUHifKltClQKkc4KKlwGD9tg6' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ]
+})
+</script>
