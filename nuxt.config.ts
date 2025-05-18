3bc55228b13d637a568750bbf9cdeb3d6274ad3b
Angel Hurtado <angelxehg@proton.me>
2025-05-18 15:18:33 -0600

    build: Print variable value

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const isNetlify = process.env.NETLIFY === 'true'
+console.log('isNetlify', isNetlify)
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   ssr: true,
@@ -7,5 +11,5 @@ export default defineNuxtConfig({
   },
   devtools: {enabled: true},
   modules: ['@nuxtjs/robots'],
-  site: {indexable: !process.env.NETLIFY} // Disable indexing on Netlify
+  site: {indexable: isNetlify} // Disable indexing on Netlify
 });
