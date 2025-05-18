b68841b26bdf50206132bb91cde357135e809acd
Angel Hurtado <angelxehg@proton.me>
2025-05-18 14:10:55 -0600

    feat: Change open graph image

--- app.vue
+++ app.vue
@@ -44,7 +44,7 @@ useHead({
     { name: 'og:site_name', content: "Angel Hurtado's portfolio" },
     { name: 'og:description', content: "Angel Hurtado's portfolio description" },
     { name: 'og:type', content: 'website' },
-    { name: 'og:image', content: 'images/OpenGraph.Opt.png' },
+    { name: 'og:image', content: 'images/og.image.png' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { name: 'google-site-verification', content: '0hO20fg4BEvRMjuTmUHifKltClQKkc4KKlwGD9tg6' }
   ],
